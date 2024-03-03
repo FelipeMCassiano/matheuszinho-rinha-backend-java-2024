239eeb935f09a452b8b84829afbc7c5618077996
FelipeMCassiano <felipemcassiano@gmail.com>
2024-03-03 20:50:27 -0300

    initalize project

---
+++ src/main/java/rinha/backendq1/BackendQ1Application.java
@@ -0,0 +1,13 @@
+package rinha.backendq1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQ1Application.class, args);
+	}
+
+}
---
+++ src/test/java/rinha/backendq1/BackendQ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rinha.backendq1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendQ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
